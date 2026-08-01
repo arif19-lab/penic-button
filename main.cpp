8c618a093901123970e8d0605c923b06ccbb9c64
arif19-lab <arif823419@gmail.com>
2026-08-01 14:08:50 +0600

    fix: convert UTF-8 to UTF-16 wide string for full Emoji, Symbol, and Unicode support in C++ SendInput

--- main.cpp
+++ main.cpp
@@ -919,20 +919,25 @@ void ProcessClient(SOCKET clientSocket) {
                     keybd_event(VK_TAB, 0, 0, 0);
                     keybd_event(VK_TAB, 0, KEYEVENTF_KEYUP, 0);
                 } else {
-                    for (char c : decodedText) {
-                        INPUT input[2] = {0};
-                        input[0].type = INPUT_KEYBOARD;
-                        input[0].ki.wVk = 0;
-                        input[0].ki.wScan = (wchar_t)c;
-                        input[0].ki.dwFlags = KEYEVENTF_UNICODE;
+                    int wlen = MultiByteToWideChar(CP_UTF8, 0, decodedText.c_str(), -1, NULL, 0);
+                    if (wlen > 1) {
+                        std::wstring wText(wlen - 1, 0);
+                        MultiByteToWideChar(CP_UTF8, 0, decodedText.c_str(), -1, &wText[0], wlen);
+                        for (wchar_t wc : wText) {
+                            INPUT input[2] = {0};
+                            input[0].type = INPUT_KEYBOARD;
+                            input[0].ki.wVk = 0;
+                            input[0].ki.wScan = wc;
+                            input[0].ki.dwFlags = KEYEVENTF_UNICODE;
 
-                        input[1].type = INPUT_KEYBOARD;
-                        input[1].ki.wVk = 0;
-                        input[1].ki.wScan = (wchar_t)c;
-                        input[1].ki.dwFlags = KEYEVENTF_UNICODE | KEYEVENTF_KEYUP;
+                            input[1].type = INPUT_KEYBOARD;
+                            input[1].ki.wVk = 0;
+                            input[1].ki.wScan = wc;
+                            input[1].ki.dwFlags = KEYEVENTF_UNICODE | KEYEVENTF_KEYUP;
 
-                        SendInput(2, input, sizeof(INPUT));
-                        Sleep(5);
+                            SendInput(2, input, sizeof(INPUT));
+                            Sleep(2);
+                        }
                     }
                 }
             }
